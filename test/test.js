6ee2124a355b3c1b3d836fd86f0c02a2e27b8dd8
hbthugo <heberthhugojob@gmail.com>
2024-09-23 14:11:00 -0300

    11

--- test/test.js
+++ test/test.js
@@ -1,4 +1,5 @@
 const assert = require('assert');
+const io = require('socket.io-client');
 
 describe('Teste Básico', function() {
   it('Deve retornar verdadeiro', function() {
@@ -6,3 +7,48 @@ describe('Teste Básico', function() {
   });
 });
 
+describe('Teste de Conexão WebSocket', () => {
+    let socket;
+
+    beforeEach((done) => {
+        socket = io('http://localhost:3000');
+        socket.on('connect', () => {
+            done();
+        });
+    });
+
+    afterEach(() => {
+        socket.close();
+    });
+
+    it('Deve conectar ao servidor WebSocket', (done) => {
+        assert.equal(socket.connected, true);
+        done();
+    });
+});
+
+describe('Teste de Envio de Mensagem', () => {
+    let socket;
+
+    beforeEach((done) => {
+        socket = io('http://localhost:3000');
+        socket.on('connect', () => {
+            done();
+        });
+    });
+
+    afterEach(() => {
+        socket.close();
+    });
+
+    it('Deve enviar e receber uma mensagem', (done) => {
+        const testMessage = 'Olá, mundo!';
+        
+        socket.emit('sendMessage', testMessage);
+
+        socket.on('receiveMessage', (message) => {
+            assert.equal(message, testMessage);
+            done();
+        });
+    });
+});
